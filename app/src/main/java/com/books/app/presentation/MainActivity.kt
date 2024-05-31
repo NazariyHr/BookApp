4dc33d255b423a5a1ceddbb265d2fc839b237b33
Nazar <nazariu11@gmail.com>
2024-06-01 00:07:45 +0300

    remove dead code

--- app/src/main/java/com/books/app/presentation/MainActivity.kt
+++ app/src/main/java/com/books/app/presentation/MainActivity.kt
@@ -12,43 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.books.app.presentation.common.theme.BookAppTheme
 import com.books.app.presentation.navigation.AppNavigation
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfig
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
-//        val configSettings = remoteConfigSettings {
-//            minimumFetchIntervalInSeconds = 3600
-//        }
-//        remoteConfig.setConfigSettingsAsync(configSettings)
-
-//        remoteConfig
-//            .fetchAndActivate()
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    val value = remoteConfig.getValue("json_data")
-//                    val config = Gson().fromJson(value.asString(), RemoteConfigResponse::class.java)
-//                    val updated = task.result
-//                    Log.d("TAG", "Config params updated: $updated")
-//                    Toast.makeText(
-//                        this,
-//                        "Fetch and activate succeeded",
-//                        Toast.LENGTH_SHORT,
-//                    ).show()
-//                } else {
-//                    Toast.makeText(
-//                        this,
-//                        "Fetch failed",
-//                        Toast.LENGTH_SHORT,
-//                    ).show()
-//                }
-//            }
-
         setContent {
             BookAppTheme {
                 // A surface container using the 'background' color from the theme
